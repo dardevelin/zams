82049d53dee6143c9bb59c0947d7c6ecaedd74d8
Darcy Brás da Silva <stardevelin@gmail.com>
2018-01-14 08:28:01 +0000

    allow environment setting for db_uri

--- zams/config.py
+++ zams/config.py
@@ -1,7 +1,7 @@
 import os
 
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "database://username:password@domain:port/database"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("ZAMS_DB_URI",
+            "database://username:password@localhost:port/database")
     DEBUG = True
     SECRET_KEY = os.environ.get("ZAMS_SECRET_KEY", os.urandom(12))
-
